b54a8950a3eadbb149cbe93536b622b677a6a6d9
Victor M. Baena <portalegre66@gmail.com>
2023-05-25 18:36:55 +0100

    End code for a lesson "Using JPA Entities".

--- src/main/java/com/victormbaena/spring6course/domain/Author.java
+++ src/main/java/com/victormbaena/spring6course/domain/Author.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Author {
 
@@ -14,27 +18,4 @@ public class Author {
     private String firstName;
     private String lastName;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
--- src/main/java/com/victormbaena/spring6course/domain/Book.java
+++ src/main/java/com/victormbaena/spring6course/domain/Book.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Book {
 
@@ -13,28 +17,4 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
 }
