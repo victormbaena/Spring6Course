33c36d0ea497f4bb0c86752bf1d4c723dacb3255
Victor M. Baena <portalegre66@gmail.com>
2023-05-25 17:55:11 +0100

    First commit with a base project.

---
+++ src/main/java/com/victormbaena/spring6course/Spring6WebappCourseApplication.java
@@ -0,0 +1,13 @@
+package com.victormbaena.spring6course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebappCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6WebappCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victormbaena/spring6course/Spring6WebappCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victormbaena.spring6course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
