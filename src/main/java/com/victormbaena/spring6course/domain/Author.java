712ee31beab80c0066c7fce6758386b4b9bea6a0
Victor M. Baena <portalegre66@gmail.com>
2023-05-26 15:43:39 +0100

    End code for a lesson "JPA Relationship and Equality in Hibernate". Section 2 lesson 16 and 18

--- src/main/java/com/victormbaena/spring6course/domain/Author.java
+++ src/main/java/com/victormbaena/spring6course/domain/Author.java
@@ -1,21 +1,35 @@
 package com.victormbaena.spring6course.domain;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Set;
+
+import static lombok.EqualsAndHashCode.Include;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 @Entity
 public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Include
     private Long id;
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
 
 }
--- src/main/java/com/victormbaena/spring6course/domain/Book.java
+++ src/main/java/com/victormbaena/spring6course/domain/Book.java
@@ -4,17 +4,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Set;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 @Entity
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Include
     private Long id;
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
 }
